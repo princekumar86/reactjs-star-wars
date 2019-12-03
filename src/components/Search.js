a174f8a4b04cf4789839c67d550dabd724965c72
princekumar <vividapps.in@gmail.com>
2019-12-03 06:23:38 +0530

    added 15 api request per min limit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Button, Nav} from 'react-bootstrap';
+import { Container, Row, Col, Button, Nav, Modal, Alert} from 'react-bootstrap';
 
 class Search extends Component {
     constructor(){
@@ -7,43 +7,89 @@ class Search extends Component {
         this.state = {
             authenticatedLoggedUser : "",
             searchKeyword : "",
-            planets : []
+            planets : [],
+            show : false,
+            planetDetail: {},
+            apiFetchRequestCounter: 0,
+            maxRequestPerUser: 15,
+            timer: ""
         }
         this.handleLogout = this.handleLogout.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
+        this.handleMoreInfo = this.handleMoreInfo.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.timer = setInterval(() => {
+            this.setState({
+                apiFetchRequestCounter: 0
+            });
+            console.log(`timer reset done after 1 minute, ${this.state.apiFetchRequestCounter}`);
+        }, 60000);
+
     }
     componentDidMount() {
         if (localStorage.getItem("authenticatedUser") === null) {
             this.props.history.push("/");
-          } else {
+        } else {
             this.setState({
                 authenticatedLoggedUser:localStorage.getItem("authenticatedUser")
             });
-          }
+        }
+    }
+    componentWillUnmount(){
+        clearInterval(this.timer);
     }
     handleLogout(){
         localStorage.removeItem("authenticatedUser");
         this.props.history.push("/");
     }
+    
     handleSearch(e){
         this.setState({
-            searchKeyword : e.target.value
-        })
+            searchKeyword : e.target.value,
+            apiFetchRequestCounter : this.state.apiFetchRequestCounter + 1
+        });
         console.log(e.target.value);
-        if(e.target.value.length >=1 ) {
-            console.log("trying fetch");
-            fetch(`https://swapi.co/api/planets/?search=${e.target.value}`)
+        if(this.state.apiFetchRequestCounter<=15) {
+            if(e.target.value.length >=1 ) {
+                fetch(`https://swapi.co/api/planets/?search=${e.target.value}`)
+                .then(response => response.json())
+                .then( data => {    
+                        console.log(data);
+                        this.setState({
+                            planets : data.results
+                        });
+                        console.log(this.state.planets);
+                    }
+                )
+                .catch(error => this.setState({ error}));
+            }
+        } else {
+            console.log('you have exceeded the limit, please try after 1 min gap, max allowed 15 api calls per min');
+        }
+    }
+    handleMoreInfo(url){
+        console.log(`handle planet more info function ${url}`);
+        if(url.length >=10 ) {
+            fetch(`${url}`)
             .then(response => response.json())
             .then( data => {    
                     console.log(data);
                     this.setState({
-                        planets : data.results
+                        planetDetail : data
                     });
-                    console.log(this.state.planets);
                 }
             )
             .catch(error => this.setState({ error}));
         }
+        this.setState({
+            show: true
+        });
+    }
+    handleClose(){
+        this.setState({
+            show: false,
+            planetDetail: {}
+        });
     }
     render() {
         return (
@@ -70,19 +116,30 @@ class Search extends Component {
                         </Col>
                     </Row>
                     <div className="search-results">
+                    {   this.state.apiFetchRequestCounter>15 && <Alert variant="danger">
+                                    You have exceeded the per min limit for searching, please try after 1 min gap
+                                </Alert>
+                                }
                         Search results...<br/>
                         {
                             this.state.planets.map( planet => (
                                         
                                 <div key={planet.name} className="card card-body">
-                                Planet <h4 className="card-title">{planet.name}</h4>
-                                <p className="card-text">
-                                    Climate: {planet.climate}<br/>
-                                    Diameter: {planet.diameter}<br/>
-                                    Population: {planet.population}<br/>
-                                    Terrain: {planet.terrain}<br/>
-                                </p>
-                                <Button href="#" className="btn btn-info">More</Button>
+                                    
+                                    <span className="symboliccircle" 
+                                    style={{width:planet.diameter/100|25, height:planet.diameter/100|25}}>
+                                    </span>
+                                    <span className="card-content">
+                                        Planet <h4 className="card-title">{planet.name}</h4>
+                                        <p className="card-text">
+                                            Climate: {planet.climate}<br/>
+                                            Diameter: {planet.diameter}<br/>
+                                            Population: {planet.population}<br/>
+                                            Terrain: {planet.terrain}<br/>
+                                        </p>
+                                        <Button href="#" className="btn btn-info" 
+                                            onClick={this.handleMoreInfo.bind(this, planet.url)}>More</Button>
+                                    </span>
                                 </div>
 
                             )
@@ -92,6 +149,27 @@ class Search extends Component {
 
                     </div>
                 </Container>
+
+                <Modal show={this.state.show}>
+                <Modal.Header>
+                    <Modal.Title>{this.state.planetDetail.name}</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <p>Climate: {this.state.planetDetail.climate}</p>
+                    <p>Population: {this.state.planetDetail.population}</p>
+                    <p>Diameter: {this.state.planetDetail.diameter}</p>
+                    <p>Terrain: {this.state.planetDetail.terrain}</p>
+                    <p>Rotation period: {this.state.planetDetail.rotation_period}</p>
+                    <p>Orbital period: {this.state.planetDetail.orbital_period}</p>
+                    <p>Gravity: {this.state.planetDetail.gravity}</p>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={this.handleClose}>OK</Button>
+                </Modal.Footer>
+                </Modal>
+
             </div>
         )
     }
